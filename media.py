1a41d7ce4ad4709a4b3936beeeaa9e34be141e44
avi <hi@avi.im>
2016-12-08 13:01:44 +0530

    Add class `Movie`

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie:
+
+    def __init__(self, title, youtube_url, poster_url):
+        self.title = title
+        self.trailer_youtube_url = youtube_url
+        self.poster_image_url = poster_url
