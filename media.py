89275480e6d145b8fcc814a331c12bfe9b8d4975
avi <hi@avi.im>
2016-12-08 13:02:03 +0530

    Add helper method to set youtube id

--- media.py
+++ media.py
@@ -4,3 +4,8 @@ class Movie:
         self.title = title
         self.trailer_youtube_url = youtube_url
         self.poster_image_url = poster_url
+
+        self._set_youtube_id()
+
+    def _set_youtube_id(self):
+        self.trailer_youtube_id = self.trailer_youtube_url.split('v=')[1]
